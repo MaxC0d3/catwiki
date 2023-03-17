19872e524aa4f8d27cf2d57c00a9f664a3a6bf73
Tobar <70650325+Tobar@users.noreply.github.com>
2023-03-16 23:33:52 -0500

    feat(login): added structure login & types declarations

--- src/app/@views/Login_/Login/Login.component.ts
+++ src/app/@views/Login_/Login/Login.component.ts
@@ -8,4 +8,6 @@ export class LoginComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+
+  
 }
---
+++ src/app/@views/Login_/Models/login.d.ts
@@ -0,0 +1,11 @@
+export interface Ilogin {
+  email: string;
+  password: string;
+}
+
+export interface IRegister{
+  username: string;
+  email: string;
+  password: string
+  date: Date;
+}
