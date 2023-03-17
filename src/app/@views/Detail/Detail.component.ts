67d826091ee1bc858420797ed2a57c318e675782
Tobar <70650325+Tobar@users.noreply.github.com>
2023-03-16 22:28:25 -0500

    feat(app): Create folder and components structure

---
+++ src/app/@views/Detail/Detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Detail',
+  templateUrl: './Detail.component.html',
+  styleUrls: ['./Detail.component.css']
+})
+export class DetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
