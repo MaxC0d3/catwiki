6f986b7ed859aba88dad02393c36ccdfd8ecfa11
MaxC0d3 <maximitianoreste@gmail.com>
2023-09-27 23:05:09 -0500

    feat(home): intro component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ViewsModule } from './@views/views.module';
 
 import { HeaderComponent } from './@components/header/header.component';
+import { IntroComponent } from './@components/intro/intro.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, IntroComponent],
   imports: [BrowserModule, AppRoutingModule, ViewsModule],
   providers: [],
   bootstrap: [AppComponent],
