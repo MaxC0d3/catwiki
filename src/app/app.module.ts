5d3a04ee15f6aa09e28555772a894b2476eccbe6
MaxC0d3 <maximitianoreste@gmail.com>
2023-09-27 22:53:20 -0500

    feat(style): header & css general

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ViewsModule } from './@views/views.module';
 
+import { HeaderComponent } from './@components/header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [BrowserModule, AppRoutingModule, ViewsModule],
   providers: [],
   bootstrap: [AppComponent],
