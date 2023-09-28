7415461ae15d25befdc6814ca5049b171b64e037
Sebastian Tobar <stonsrider@gmail.com>
2023-09-27 23:11:37 -0500

    Refactor(app): Remove module files

--- src/app/@views/Home_/home-module.module.ts
+++
@@ -1,9 +0,0 @@
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-import { HomeComponent } from './Home/Home.component';
-
-@NgModule({
-  declarations: [HomeComponent],
-  imports: [CommonModule],
-})
-export class HomeModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ViewsModule } from './@views/views.module';
 
 import { HeaderComponent } from './@components/header/header.component';
 import { IntroComponent } from './@components/intro/intro.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './@views/Home_/Home/Home.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, IntroComponent],
-  imports: [BrowserModule, AppRoutingModule, ViewsModule],
+  declarations: [AppComponent, HeaderComponent, IntroComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
