3fa0e8d60b673813a449e70bc6b4caf1134fe965
MaxC0d3 <maximitianoreste@gmail.com>
2023-09-28 00:05:00 -0500

    feat(home): intro/hero

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { IntroComponent } from './@components/intro/intro.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './@views/Home_/Home.component';
+import { SearchComponent } from './@components/search/search.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, IntroComponent, HomeComponent],
+  declarations: [AppComponent, HeaderComponent, IntroComponent, HomeComponent, SearchComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
